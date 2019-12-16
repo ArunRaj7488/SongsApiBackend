bcdb728a704e00c3265833b880e8dcc2186df660
Arun Gautam <arunraj1735169@gmail.com>
2019-12-16 22:43:03 +0530

    heroku

---
+++ db.js
@@ -0,0 +1,27 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/song')
+.then(() => console.log('connected to mongodb'))
+.catch(err => console.log('mongodb not connected ...', err));
+
+const createSchema = new mongoose.Schema({
+	songName: String,
+	artist: String,
+	movie: String,
+	year: Number
+});
+
+const Song = mongoose.model("Song", createSchema);
+
+function validateSchema(song) {
+	const shema = {
+		songName: Joi.string().required(),
+		artist: Joi.string().required(),
+		movie: Joi.string().required(),
+		year: Joi.string().required(),
+	}
+	Joi.validate(song, validateSchema)
+}
+exports.validateSchema = validateSchema;
+exports.Song = Song;
---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const {Song, validateSchema} = require('./db');
+
+app.use(cors());
+
+app.use(express.json());
+
+
+app.get('/',  async(req, res) => {
+	const song = await Song.find();
+	res.send(song)
+});
+
+app.post('/', cors(),  async(req,res) =>{
+
+		const { error } = validateSchema(req.body);
+		if(error) return res.status(404).send(error.details[0].message);
+		const songName = await Song.findOne({songName: req.body.songName});
+		if(songName) return res.status(400).send('song is already exist');
+
+		const song = new Song(req.body);
+
+		await song.save(song);
+		res.send(song);
+
+
+})
+
+port = 5000;
+app.listen(port,() => console.log(`listning on port ${port}...`));
